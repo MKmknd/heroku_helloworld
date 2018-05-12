1054fd1105d6a1db15b824a6fb09bd38797e2fdf
Masanari Kondo <masa@d454.cs.queensu.ca>
2018-05-12 13:17:47 -0400

    [fix] main.py and requirements

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+from flask import Flask
 
-print('Hello World')
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_wold():
+
+    return 'Hello World'
+
+if __name__=='__main__':
+    app.run()
